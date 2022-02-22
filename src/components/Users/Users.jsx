27a772d19ab834133fab80b6ae4763bc6442dc19
Shad Rais <shadrais321@outlook.com>
2022-02-22 16:52:41 +0530

    v1.1

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -13,6 +13,7 @@ const Users = () => {
   useEffect(() => {
     getUsers(params.login)
     getRepos(params.login)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const {
